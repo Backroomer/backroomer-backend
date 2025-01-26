a46ca5691f127fdbfc8015fbe210b8faa4ba4563
azureact <163717172+azureact@users.noreply.github.com>
2025-01-26 12:03:20 +0800

    简化selector.rs

--- src/selectors.rs
+++ src/selectors.rs
@@ -1,20 +1,26 @@
 use lazy_static::lazy_static;
 use scraper::Selector;
 
-lazy_static! {
-    pub static ref PRINTUSER: Selector = Selector::parse("span.printuser").unwrap();
-    pub static ref VOTE: Selector = Selector::parse("span[style^='color']").unwrap();
-    pub static ref PAGESOURCE: Selector = Selector::parse("div.page-source").unwrap();
-    pub static ref TR: Selector = Selector::parse("tr").unwrap();
-    pub static ref TD: Selector = Selector::parse("td").unwrap();
-    pub static ref ODATE: Selector = Selector::parse("span.odate").unwrap();
-    pub static ref A: Selector = Selector::parse("a").unwrap();
-    pub static ref PAGE: Selector = Selector::parse("div.page").unwrap();
-    pub static ref STAR: Selector = Selector::parse("span.rating span.page-rate-list-pages-start").unwrap();
-    pub static ref SET: Selector = Selector::parse("span.set").unwrap();
-    pub static ref KEY: Selector = Selector::parse("span.name").unwrap();
-    pub static ref VALUE: Selector = Selector::parse("span.value").unwrap();
-    pub static ref PAGERNO: Selector = Selector::parse("span.pager-no").unwrap();
-    pub static ref H1: Selector = Selector::parse("h1").unwrap();
-    pub static ref IMG: Selector = Selector::parse("img").unwrap();
+macro_rules! build_selector {
+    ($name: ident, $selector: expr) => {
+        lazy_static! {
+            pub static ref $name: Selector = Selector::parse($selector).unwrap();
+        }
+    };
 }
+
+build_selector!(PRINTUSER, "span.printuser");
+build_selector!(VOTE, "span[style^='color']");
+build_selector!(PAGESOURCE, "div.page-source");
+build_selector!(TR, "tr");
+build_selector!(TD, "td");
+build_selector!(ODATE, "span.odate");
+build_selector!(A, "a");
+build_selector!(PAGE, "div.page");
+build_selector!(STAR, "span.rating span.page-rate-list-pages-start");
+build_selector!(SET, "span.set");
+build_selector!(KEY, "span.name");
+build_selector!(VALUE, "span.value");
+build_selector!(PAGERNO, "span.pager-no");
+build_selector!(H1, "h1");
+build_selector!(IMG, "img");
